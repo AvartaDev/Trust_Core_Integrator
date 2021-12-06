744ed6ffc550a37250baa5dc459ddcf89939b32a
Zh Jing <zh.jing0105@outlook.com>
2021-12-06 11:46:31 +0700

    BTC create-wallet

--- wrapper.py
+++ wrapper.py
@@ -1,14 +1,26 @@
 from flask import Flask, request
 import json
+from waitress import serve
+import walletcore as core
+
 
 wrapper_server = Flask(__name__)
 
-@wrapper_server.route('/transfer', methods=['POST'])
-def transfer():
+@wrapper_server.route('/bitcoin/create-wallet', methods=['POST'])
+def bitcoin_create_wallet():
+    wallet = core.HDWallet.create(128, 'abcdefghijklmnopqrstuvwxyz')
+    mnemonic = wallet.mnemonic
+    entropy = wallet.entropy
+    address = wallet.get_address_for_coin(core.CoinType.Bitcoin)
+
+    return json.dumps({"status": True, "BTC wallet address": address, "mnemonic": mnemonic})
+
+@wrapper_server.route('/bitcoin/sign-transaction', methods=['POST'])
+def bitcoin_sign_transaction():
     postData = request.json
-    print(postData)
     msg = postData.get('message')
 
-    socketio.emit('message', {'msg': msg})
+    return json.dumps({"status": True, "BTC wallet address": address, "mnemonic": mnemonic})
 
-    return json.dumps({"status": True})
+# wrapper_server.run(port = 3000)
+serve(wrapper_server, host="0.0.0.0", port=8080)
